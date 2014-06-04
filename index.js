93f9798c02838b899ffba4e2d6e0e34028bce8a7
Gabriel Zea <zea@randomlab.net>
2014-06-03 21:34:25 -0500

    basic folder selector

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 var Server = require('cartapacio-server'),
-    gui = require('nw.gui') 
-
-var server = new Server()
-server.bootstrap()
+    gui = require('nw.gui')
 
 var win = gui.Window.get()
-win.window.location = "admin/index.html"
 
-console.log("---Cartpacio Ready") 
+
+try{
+  var config = require('./localConfig.json')
+  console.log('root path: ' + config.path)
+
+  var server = new Server(config.path)
+  server.init()
+
+
+  win.window.location = 'admin/index.html'
+
+  console.log('---Cartpacio Ready')
+
+} catch (err) {
+  // throw new Error('config file not found')
+  win.window.location = 'folderConfig/index.html'
+}
+
