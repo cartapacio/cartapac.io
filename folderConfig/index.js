4d53c91fc8a62f8e771f7300d09bea2bbb505dc7
Gabriel Zea <zea@randomlab.net>
2014-07-02 12:39:27 -0500

    native folder selection for project

--- folderConfig/index.js
+++ folderConfig/index.js
@@ -1,147 +1,43 @@
 'use strict';
 
 var fs = require('fs-extra'),
-  path = require('path'),
-  async = require('async'),
-  $ = require('jquery'),
-  Handlebars = require('handlebars')
+  $ = require('jquery')
 
+function save (folderPath) {
+  var configFile = 'localConfig.json'
 
-var source = '<li class="folder" data-path="{{up}}">'+
-  '<i class="fi-arrow-up"></i>'+
-'</li>'+
-'{{#each content}}'+
-'  <li>'+
-    '<i class="fi-folder"></i>'+
-  '<span class="folder" data-path="{{this.path}}/{{this.name}}">{{this.name}}</span>'+
-  '<span class="select" data-path="{{this.path}}/{{this.name}}">'+
-    '<i class="fi-check"></i>'+
-  '</span>'+
-  '</li>'+
-'{{/each}}'
-
-var template = Handlebars.compile(source)
-
-var destinationFolder = null
-
-function save () {
-  $('#msg').text('')
-  console.log('here');
-  if(destinationFolder){
-    var folder = destinationFolder,
-      root =  process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE,
-      fullPath = path.join(root, folder, 'website'),
-      //configFile = path.join('..', 'localConfig.json')
-      configFile = 'localConfig.json'
-    async.waterfall([
-      function (next){
-        fs.ensureDir(fullPath, function (err){
-          if(err){
-            next(err)
-          }
-          next()
-        })
-      },
-      function (next){
-        fs.outputFile(configFile, JSON.stringify({path:fullPath}), function (err){
-          if(err){
-            next(err)
-          }
-          next()
-        })
-      }
-      ],
-      function (err){
-        if(err){
-          throw err
-        }
-        window.location = '../index.html'
-    })
-  } else {
-    $('#msg').text('no folder selected!')
+  var data = {
+    path: folderPath
   }
-}
 
-
-function update(e){
-  var path = $(e).attr('data-path')
-  exposeFolder(path, function (data){
-     $('#folderList').html(template(data))
-  })
-}
-
-function select(e){
-  destinationFolder = $(e).attr('data-path')
-  $('li').removeClass('selected')
-  $(e).closest('li').toggleClass('selected')
-}
-
-function exposeFolder (requestPath, next) {
-
-  var root =  process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE,
-    folder = requestPath || '',
-    fullPath = path.join(root, folder),
-    excludeDot = /^([^.][\w\W]+)/,
-    folderList = new Array()
-
-  fs.readdir(fullPath, function (err, files){
-    if (err){
-      throw err
+  fs.outputFile(configFile, JSON.stringify(data), function (err){
+    if(err){
+      console.error(err)
     }
 
-    async.forEach(files,
-      function (file, callback){
-        if(excludeDot.exec(file)){
-          var current = path.join(fullPath, file)
-          fs.lstat(current, function (err, stats){
-            if(err){
-              callback(err)
-            }
-            if(stats.isDirectory()){
-              folderList.push({
-                name: file,
-                path: folder
-              })
-            }
-            callback(null)
-          })
-        } else {
-          callback(null)
-        }
-      },
-      function(err){
-        if(err){
-          throw err
-        }
-        next({
-          up: path.resolve(folder, '..'),
-          content: folderList
-        })
-      })
+    window.location = '../index.html'
   })
 }
 
 $(function () {
-  exposeFolder('', function (data){
-    console.log(data);
-    var e = $('#folderList')
-    e.html(template(data))
+  var chooser = $('#folder-input');
 
+  chooser.on('change', function() {
+      var selection = $(this).val()
 
-  })
+      if(selection){
+        save(selection)
+      } else {
+        console.log('empty path');
+      }
+    });
 
-  $(document).on('click', '.folder', function (){
-    update(this);
-  })
-
-  $(document).on('click', '.select', function (){
-    select(this)
-  })
-
-  $(document).on('click', '#save', function (){
-    save(this)
+  $('#select').click(function (){
+    chooser.trigger('click');
   })
 });
+
+
 
 
 
